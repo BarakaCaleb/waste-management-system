61675bc315269f08abba27c3770a4a63eca44009
BarakaCaleb <calebbaraka79@gmail.com>
2025-06-07 20:38:18 +0300

    updates

--- controllerUserData.php
+++ controllerUserData.php
@@ -18,8 +18,8 @@ function sendMail($to, $subject, $body) {
         $mail->isSMTP();
         $mail->Host       = 'smtp.gmail.com';
         $mail->SMTPAuth   = true;
-        $mail->Username   = 'calebbaraka79gmail@gmail.com'; // Replace with your Gmail
-        $mail->Password   = 'yzfl oiac mgde pyvr';   // Replace with your App Password
+        $mail->Username   = 'calebbaraka79@gmail.com'; // Replace with your Gmail
+        $mail->Password   = 'yzfloiacmgdepyvr';   // Replace with your App Password
         $mail->SMTPSecure = 'tls';
         $mail->Port       = 587;
 
